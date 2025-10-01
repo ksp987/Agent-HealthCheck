38f7717ebdfaed4d3d52c5a793854a08879c1aa4
Sai praveen Kallepalli <saipraveen98@outlook.com>
2025-10-02 09:31:20 +1000

    Run Unit test -7 (email_processor)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,7 @@ from dotenv import load_dotenv
 @pytest.fixture(autouse=True, scope="session")
 def load_env():
     load_dotenv()  # loads .env automatically for all tests
+
+@pytest.fixture(autouse=True)
+def fake_openai_client(mocker):
+    mocker.patch("src.infrastructure.email_processor.OpenAI", autospec=True)
